34c7e2ff3650ad7b32eaf032036d3eaaa5d5accb
Gabe <gabe@paperlesspost.com>
2014-10-16 00:35:59 -0400

    actually link to google maps

--- js/gabeandluz.js
+++ js/gabeandluz.js
@@ -1,9 +1,9 @@
 (function($){
-  $('.js-open-event-info-modal').click(function () {
+  $('.js-open-event-info-modal').click(function (e) {
     $('.event-info-modal').fadeIn('fast');
+    return false;
   });
-
-  $('.modal-background').click(function () {
+  $('.modal-background').click(function (e) {
     $('.event-info-modal').fadeOut('fast');
   });
 })(jQuery);
